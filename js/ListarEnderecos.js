b064e9fc638e522f8cc65d977ca35511049ae8c3
Fábio Garrote <fgarrote12@gmail.com>
2024-11-18 13:21:48 -0300

    Projeto final da matéria de Desenvolvimento Web da faculdade Impacta

---
+++ js/ListarEnderecos.js
@@ -0,0 +1,63 @@
+const url = 'https://go-wash-api.onrender.com/api/auth/address'
+let token = JSON.parse(localStorage.getItem('user')).access_token
+
+async function listarEndereco() {
+    let table = document.getElementById('enderecos')
+
+    let api = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        })
+    
+    if (api.ok) {
+        let resposta = await api.json()
+
+        table.innerHTML = `
+            <tr id="legenda-tabela">
+                <th>ID</th>
+                <th>Apelido</th>
+                <th>CEP</th>
+                <th>Endereço</th>
+                <th>Ação</th>
+            </tr>
+        `
+
+        resposta.data.forEach((endereco) => {
+            table.innerHTML += `
+                <tr>
+                    <th>${endereco.id}</th>
+                    <th>${endereco.title}</th>
+                    <th>${endereco.cep}</th>
+                    <th>${endereco.address}</th>
+                    <th id="btns">
+                        <input type="button" value="Atualizar" class="btn-tabela" onclick="window.location.assign('./AtualizarEndereco.html?id=${endereco.id}')">
+                        <input type="button" value="Deletar" class="btn-tabela" onclick="excluirEndereco(${endereco.id})">
+                    </th>
+                </tr>
+            `
+        })
+    }
+}
+
+async function excluirEndereco(id) {
+    let deletarEndereco = confirm('Você quer mesmo deletar esse endereço?')
+
+    if (deletarEndereco) {
+        let api = await fetch(`${url}/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        })
+
+        if (api.ok) {
+            alert('Endereço deletado')
+            listarEndereco()
+            return
+        }
+    }
+}
+
+listarEndereco()
